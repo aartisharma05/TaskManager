86b9e7c0a879b78ba3e1a09cc1c686b95d5573ad
aartisharma05 <aartisharma.official1@gmail.com>
2024-03-20 22:28:00 +0530

    "sidebar"

--- mytasks/app/components/Sidebar/Sidebar.tsx
+++ mytasks/app/components/Sidebar/Sidebar.tsx
@@ -2,11 +2,32 @@
 
 import styled from "styled-components";
 import { useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
 
 export default function Sidebar() {
   const theme = useGlobalState();
   console.log(theme);
-  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+  return (
+    <SidebarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay">
+          <div className="image">
+            <Image
+              width={70}
+              height={70}
+              src="/avatar1.png"
+              alt="profile"
+            ></Image>
+          </div>
+          <h1>
+            <span>Sin</span>
+            <span>Doe</span>
+          </h1>
+        </div>
+        <ul className="nav-items"></ul>
+      </div>
+    </SidebarStyled>
+  );
 }
 
 const SidebarStyled = styled.nav`
--- mytasks/app/providers/ContextProvider.tsx
+++ mytasks/app/providers/ContextProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import { GlobalProvider } from "../context/globalProvider";
 
@@ -8,5 +8,17 @@ interface Props {
   children: React.ReactNode;
 }
 export default function ContextProvider({ children }: Props) {
+  const [isReady, setIsReady] = React.useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 200);
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return <GlobalProvider>{children}</GlobalProvider>;
 }
